adf10defb5b39f90e7a3ab2c79f1c8e38373e710
Trinh Hoang Anh <trha@outlook.com>
2017-08-05 23:36:59 +0800

    Virtual desktop aware

--- wtile/main.cpp
+++ wtile/main.cpp
@@ -1,9 +1,13 @@
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <memory>
 #include <type_traits>
 #include <fmt/format.h>
 #include <vector>
-#include <winuser.h>
+#include <shlobj.h>
+#include <atlbase.h>
+#include <comutil.h>
 
 auto make_unique_hook(HHOOK h) {
   return std::unique_ptr<std::remove_pointer_t<HHOOK>, decltype(&UnhookWindowsHookEx)>{h, &UnhookWindowsHookEx};
@@ -46,23 +50,31 @@ bool move_divvy_to_cursor() {
   return true;
 }
 
-std::vector<HWND> top_level_wnds() {
-  std::vector<HWND> r;
+std::vector<HWND> top_level_wnds(IVirtualDesktopManager* mgr) {
+  struct State{
+    IVirtualDesktopManager* mgr;
+    std::vector<HWND> r;
+  } state{mgr};
+
   EnumWindows([] (HWND wnd, LPARAM lp) {
+    auto& state = *reinterpret_cast<State*>(lp);
     WINDOWPLACEMENT placement;
     placement.length = sizeof(placement);
     wchar_t title[255]{};
+    BOOL is_on_current_desktop;
 
-    if (IsWindowVisible(wnd) && GetWindowPlacement(wnd, &placement) && ((placement.showCmd & SW_SHOWNORMAL) || (placement.showCmd & SW_SHOWMAXIMIZED)) && GetWindowTextW(wnd, title, std::size(title)) && title[0] && wcscmp(title, L"Program Manager") != 0) {
-      reinterpret_cast<decltype(r)*>(lp)->push_back(wnd);
+    if (IsWindowVisible(wnd) 
+      && SUCCEEDED(state.mgr->IsWindowOnCurrentVirtualDesktop(wnd, &is_on_current_desktop)) && is_on_current_desktop 
+      && GetWindowPlacement(wnd, &placement) && ((placement.showCmd & SW_SHOWNORMAL) || (placement.showCmd & SW_SHOWMAXIMIZED)) 
+      && GetWindowTextW(wnd, title, std::size(title)) && title[0] && wcscmp(title, L"Program Manager") != 0) {
+      state.r.push_back(wnd);
     }
     return TRUE;
-  }, reinterpret_cast<LPARAM>(&r));
-  return r;
+  }, reinterpret_cast<LPARAM>(&state));
+  return state.r;
 }
 
-void shift_focus(Dir dir) {
-  auto wnds = top_level_wnds();
+void shift_focus(std::vector<HWND> wnds, Dir dir) {
   auto b = cbegin(wnds);
   auto e = cend(wnds);
   if (b == e) {
@@ -94,25 +106,25 @@ void shift_focus(Dir dir) {
 
     switch (dir) {
     case Dir::h: 
-    if (rc.left < fg_rc.left && std::abs(rc.top - fg_rc.top) < std::abs(best.rc.top - fg_rc.top)) {
+    if (rc.left < fg_rc.left && (best.rc.top < 1 || std::abs(rc.top - fg_rc.top) < std::abs(best.rc.top - fg_rc.top))) {
       best = Best{ wnd, rc };
     }
     break;
 
     case Dir::j: 
-    if (rc.top > fg_rc.top && std::abs(rc.left - fg_rc.left) < std::abs(best.rc.left - fg_rc.left)) {
+    if (rc.top > fg_rc.top && (best.rc.top < 1 || std::abs(rc.left - fg_rc.left) < std::abs(best.rc.left - fg_rc.left))) {
       best = Best{ wnd, rc };
     }
     break;
 
     case Dir::k: 
-    if (rc.top < fg_rc.top && std::abs(rc.left - fg_rc.left) < std::abs(best.rc.left - fg_rc.left)) {
+    if (rc.top < fg_rc.top && (best.rc.top < 1 || std::abs(rc.left - fg_rc.left) < std::abs(best.rc.left - fg_rc.left))) {
       best = Best{ wnd, rc };
     }
     break;
 
     case Dir::l: 
-    if (rc.left > fg_rc.left && std::abs(rc.top - fg_rc.top) < std::abs(best.rc.top - fg_rc.top)) {
+    if (rc.left > fg_rc.left && (best.rc.top < 1 || std::abs(rc.top - fg_rc.top) < std::abs(best.rc.top - fg_rc.top))) {
       best = Best{ wnd, rc };
     }
     break;
@@ -123,8 +135,24 @@ void shift_focus(Dir dir) {
     SetForegroundWindow(best.wnd);
   }
 }
+const CLSID CLSID_ImmersiveShell = {
+  0xC2F03A33, 0x21F5, 0x47FA, 0xB4, 0xBB, 0x15, 0x63, 0x62, 0xA2, 0xF2, 0x39 };
 
 int APIENTRY wWinMain(HINSTANCE inst, HINSTANCE, LPWSTR, int) {
+  if (FAILED(CoInitialize(nullptr))) {
+    return -1;
+  }
+
+  CComPtr<IServiceProvider> shell;
+  if (FAILED(shell.CoCreateInstance(CLSID_ImmersiveShell, nullptr, CLSCTX_LOCAL_SERVER))) {
+    return -2;
+  }
+
+  CComPtr<IVirtualDesktopManager> desktop_mgr;
+  if (FAILED(shell->QueryService(__uuidof(IVirtualDesktopManager), &desktop_mgr))) {
+    return -3;
+  }
+
   auto kb_hook = make_unique_hook(SetWindowsHookExW(WH_KEYBOARD_LL, [] (int code, WPARAM wp, LPARAM lp) -> LRESULT {
     auto vk = reinterpret_cast<KBDLLHOOKSTRUCT*>(lp)->vkCode;
     if (wp == WM_KEYUP) {
@@ -192,7 +220,7 @@ int APIENTRY wWinMain(HINSTANCE inst, HINSTANCE, LPWSTR, int) {
     divvy.activated = false;
     break;
     case my_msg::shift_focus: 
-    shift_focus(static_cast<Dir>(m.wParam));
+    shift_focus(top_level_wnds(desktop_mgr.p), static_cast<Dir>(m.wParam));
     break;
 
     }
